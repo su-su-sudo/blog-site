3a96a16cf7ec219747e84fc363650b92ccf85e97
Ian Aaberg <ian@Ians-MacBook-Pro.local>
2019-10-23 12:30:53 -0400

    Add gitignore, procfile update ports

--- app.js
+++ app.js
@@ -80,7 +80,10 @@ app.get('/posts/:postId', function(req, res){
       });
 
     });
-
-    app.listen(3000, function() {
-      console.log('Server now active on port 3000');
+  let port = process.env.PORT;
+  if (port == null || port == ""){
+    port = 3000;
+  }
+    app.listen(port, function() {
+      console.log('Server now active');
     });
